e64e9fd5e5420512636ac02ea6cd2c915115ec10
swang <mingisme@126.com>
2019-08-16 11:00:56 +0800

    init

---
+++ deploy-lit/modules/deploy-modules.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+mkdir -p /opt/jboss/keycloak/modules/system/layers/base/com/swang/keycloak/fakesso/main
+cd /opt/jboss/keycloak/modules/system/layers/base/com/swang/keycloak/fakesso/main
+cp /opt/jboss/modules/fakesso/* .
+
+mkdir -p /opt/jboss/keycloak/modules/system/layers/base/com/swang/keycloak/userstorage/main
+cd /opt/jboss/keycloak/modules/system/layers/base/com/swang/keycloak/userstorage/main
+cp /opt/jboss/modules/userstorage/* .
+
+cd /opt/jboss/keycloak
+
+bin/jboss-cli.sh --file=/opt/jboss/modules/cli/standalone-configuration.cli
+rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
+
+bin/jboss-cli.sh --file=/opt/jboss/modules/cli/standalone-ha-configuration.cli
+rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
+
+touch /opt/jboss/keycloak/standalone/configuration/example-users.properties
